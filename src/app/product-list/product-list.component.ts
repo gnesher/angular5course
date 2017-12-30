e68d0f1dbf8a0fbf8b2590d3e9532e30d9ef44c7
gnesher <nesher.guy@gmail.com>
2017-12-30 20:10:26 +0200

    add category filters

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
+import _ from 'lodash';
 export interface ProductCategory {
   id: number;
   title: string;
@@ -22,34 +22,56 @@ export interface Product {
 export class ProductListComponent implements OnInit {
 
   selectedProduct: Product = null;
+  selectedCategory = 0;
 
-  categories: ProductCategory[] = [
-    {id: 1, title: 'first category'},
-    {id: 2, title: 'second category'},
-  ];
+  categories: ProductCategory[];
 
-  itemList: Product[] = [
-    { title: 'Item 1', price: 1000, categoryId: 1, image: '/assets/img1.jpg', description: 'item 1' },
-    { title: 'Item 2', price: 1100, categoryId: 1, image: '/assets/img2.jpg', description: 'item 2' },
-    { title: 'Item 3', price: 1200, categoryId: 2, image: '/assets/img3.jpg', description: 'item 3' },
-    { title: 'Item 4', price: 1300, categoryId: 2, image: '/assets/img4.jpg', description: 'item 4' },
-    { title: 'Item 5', price: 1400, categoryId: 2, image: '/assets/img5.jpg', description: 'item 5' },
-    { title: 'Item 1', price: 1000, categoryId: 1, image: '/assets/img1.jpg', description: 'item 1' },
-    { title: 'Item 2', price: 1100, categoryId: 1, image: '/assets/img2.jpg', description: 'item 2' },
-    { title: 'Item 3', price: 1200, categoryId: 2, image: '/assets/img3.jpg', description: 'item 3' },
-    { title: 'Item 4', price: 1300, categoryId: 2, image: '/assets/img4.jpg', description: 'item 4' },
-    { title: 'Item 5', price: 1400, categoryId: 2, image: '/assets/img5.jpg', description: 'item 5' },
-    { title: 'Item 1', price: 1000, categoryId: 1, image: '/assets/img1.jpg', description: 'item 1' },
-    { title: 'Item 2', price: 1100, categoryId: 1, image: '/assets/img2.jpg', description: 'item 2' },
-    { title: 'Item 3', price: 1200, categoryId: 2, image: '/assets/img3.jpg', description: 'item 3' },
-    { title: 'Item 4', price: 1300, categoryId: 2, image: '/assets/img4.jpg', description: 'item 4' },
-    { title: 'Item 5', price: 1400, categoryId: 2, image: '/assets/img5.jpg', description: 'item 5' },
-  ];
+  itemList: Product[];
+  filteredItems: Product[];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.itemList = [
+      { title: 'Item 1', price: 1000, categoryId: 1, image: '/assets/img1.jpg', description: 'item 1' },
+      { title: 'Item 2', price: 1100, categoryId: 1, image: '/assets/img2.jpg', description: 'item 2' },
+      { title: 'Item 3', price: 1200, categoryId: 2, image: '/assets/img3.jpg', description: 'item 3' },
+      { title: 'Item 4', price: 1300, categoryId: 2, image: '/assets/img4.jpg', description: 'item 4' },
+      { title: 'Item 5', price: 1400, categoryId: 2, image: '/assets/img5.jpg', description: 'item 5' },
+      { title: 'Item 1', price: 1000, categoryId: 1, image: '/assets/img1.jpg', description: 'item 1' },
+      { title: 'Item 2', price: 1100, categoryId: 1, image: '/assets/img2.jpg', description: 'item 2' },
+      { title: 'Item 3', price: 1200, categoryId: 2, image: '/assets/img3.jpg', description: 'item 3' },
+      { title: 'Item 4', price: 1300, categoryId: 2, image: '/assets/img4.jpg', description: 'item 4' },
+      { title: 'Item 5', price: 1400, categoryId: 2, image: '/assets/img5.jpg', description: 'item 5' },
+      { title: 'Item 1', price: 1000, categoryId: 1, image: '/assets/img1.jpg', description: 'item 1' },
+      { title: 'Item 2', price: 1100, categoryId: 1, image: '/assets/img2.jpg', description: 'item 2' },
+      { title: 'Item 3', price: 1200, categoryId: 2, image: '/assets/img3.jpg', description: 'item 3' },
+      { title: 'Item 4', price: 1300, categoryId: 2, image: '/assets/img4.jpg', description: 'item 4' },
+      { title: 'Item 5', price: 1400, categoryId: 2, image: '/assets/img5.jpg', description: 'item 5' },
+    ];
+
+    this.categories = [
+      {id: 0, title: 'all'},
+      {id: 1, title: 'first category'},
+      {id: 2, title: 'second category'},
+    ];
+
+    this.filterList(0);
+  }
+
+  filterList(categoryID) {
+    this.selectedCategory = categoryID;
+    this.setFilteredItems();
+  }
+
+  setFilteredItems() {
+    this.filteredItems = _.filter(this.itemList, (item) => {
+      if (this.selectedCategory === 0) {
+        return item;
+      }
+      return item.categoryId === this.selectedCategory;
+    });
   }
 
 }
